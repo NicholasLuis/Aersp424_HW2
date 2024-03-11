92a5d6345fb06fdba74ebf43b7ac1619165983eb
Nicholas Luis <nml5604@psu.edu>
2024-03-11 12:19:05 -0400

    Finished 1.1

---
+++ Problem1_Classes.cpp
@@ -0,0 +1,8 @@
+#include "Problem1_Classes.h"
+
+// This program contains all of the necessary classes used in question 1
+
+Sensor::Sensor()
+{
+	
+}
---
+++ Problem1_Classes.h
@@ -0,0 +1,16 @@
+#ifndef Problem1_Classes_H
+#define Problem1_Classes_H
+
+// This program contains all of the necessary classes used in question 1
+
+// Problem 1.1
+class Sensor
+{
+private:
+
+public:
+	virtual void gatherData() = 0;
+};
+
+
+#endif

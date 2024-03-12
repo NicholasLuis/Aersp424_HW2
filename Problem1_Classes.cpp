27dcd65f1bbfbf5d8c0b047b71c08060a74fc913
Nicholas Luis <nml5604@psu.edu>
2024-03-12 15:10:13 -0400

    Working on 1.3

--- Problem1_Classes.cpp
+++ Problem1_Classes.cpp
@@ -1,8 +1,35 @@
 #include "Problem1_Classes.h"
 
-// This program contains all of the necessary classes used in question 1
+// This program contains all of the necessary functions for the classes used in question 1
 
-Sensor::Sensor()
+
+
+// Question 1.2
+void Altimeter::gatherData() {
+	std::cout << "Gathering data from Altimeter sensor" << std::endl;
+};
+void Altimeter::processData() {
+	std::cout << "Processing data from Altimeter sensor" << std::endl;
+};
+
+void IMU::gatherData() {
+	std::cout << "Gathering data from IMU sensor" << std::endl;
+};
+void IMU::processData() {
+	std::cout << "Processing data from IMU sensor" << std::endl;
+};
+
+void Thermocouple::gatherData() {
+	std::cout << "Gathering data from TC sensor" << std::endl;
+};
+void Thermocouple::processData() {
+	std::cout << "Processing data from TC sensor" << std::endl;
+};
+
+
+// Question 1.3
+Sensor* SensorFactory::createSensor(std::string inputStr)
 {
-	
+	Sensor* sensor = new Sensor();
+	return &sensor;
 }
--- Problem1_Classes.h
+++ Problem1_Classes.h
@@ -1,16 +1,35 @@
 #ifndef Problem1_Classes_H
 #define Problem1_Classes_H
 
+#include <iostream>
+
 // This program contains all of the necessary classes used in question 1
 
 // Problem 1.1
-class Sensor
-{
-private:
-
+class Sensor {
 public:
 	virtual void gatherData() = 0;
+	virtual void processData() = 0;
 };
 
+// Problem 1.2
+class Altimeter : public Sensor { 
+	void gatherData(); 
+	void processData();
+};
+class IMU : public Sensor {
+	void gatherData();
+	void processData();
+};
+class Thermocouple : public Sensor {
+	void gatherData();
+	void processData();
+};
+
+class SensorFactory {
+	static Sensor* createSensor(std::string inputStr) {
+		
+	}
+};
 
 #endif
